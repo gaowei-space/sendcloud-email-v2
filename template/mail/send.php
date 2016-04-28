4d1c8ace6ed43305baa7411099f953a15b062129
Shevy <949881439@qq.com>
2016-04-28 13:40:29 +0800

    删除敏感信息
    删除敏感信息

--- template/mail/send.php
+++ template/mail/send.php
@@ -2,20 +2,20 @@
 
 return [
 	'welcome' => [
-		'from'          => '574630774@qq.com', # 发信人，用正确邮件地址替代
-		'fromName'      => '来自Shevy的一份信',
-		'to'            => '18001210160@sina.cn',# 收件人地址, 用正确邮件地址替代, 多个地址用';'分隔  
-		'subject'       => '欢迎使用ASO100，完善信息有奖励噢！',
-		'html'          => '欢迎使用ASO100，完善信息有奖励噢！你不来耍一耍么？<a href="http://aso100.com">去完善>></a>',
+		'from'          => '***@qq.com', # 发信人，用正确邮件地址替代
+		'fromName'      => '来自***的一份信',
+		'to'            => '***@sina.cn',# 收件人地址, 用正确邮件地址替代, 多个地址用';'分隔  
+		'subject'       => '欢迎使用***，完善信息有奖励噢！',
+		'html'          => '欢迎使用***，完善信息有奖励噢！你不来耍一耍么？<a href="http://***.com">去完善>></a>',
 		'respEmailId'   => 'true',      // ('true'|'false')
 	],
 	'welcomeBatch' => [
-		'from'               => '574630774@qq.com', # 发信人，用正确邮件地址替代
-		'fromName'           => '来自ASO100的一份信',
-		'to'            	 => 'daren@maillist.sendcloud.org',#  地址列表的个数不能超过 5,多个地址用';'分隔  
-		'subject'            => '欢迎使用ASO100，完善信息有奖励噢！',
-		'html'          	 => '欢迎使用ASO100，完善信息有奖励噢！你不来耍一耍么？<a href="http://aso100.com">去完善>></a>',
-		'useAddressList'	 => 'true',
+		'from'               => '***@qq.com', # 发信人，用正确邮件地址替代
+		'fromName'           => '来自***的一份信',
+		'to'                 => '***@maillist.sendcloud.org',#  地址列表的个数不能超过 5,多个地址用';'分隔  
+		'subject'            => '欢迎使用***，完善信息有奖励噢！',
+		'html'               => '欢迎使用***，完善信息有奖励噢！你不来耍一耍么？<a href="http://***.com">去完善>></a>',
+		'useAddressList'     => 'true',
 		'respEmailId'        => 'true',      // ('true'|'false')
 	],
  ];
